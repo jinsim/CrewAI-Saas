2b7e5356e63c74521ea511945d14c50b1da8c6d0
june0216 <ziyun1612@ewhain.net>
2024-08-04 00:27:36 +0900

    chat crud 작성

--- crewai_saas/crud/crud_employed_crew.py
+++ crewai_saas/crud/crud_employed_crew.py
@@ -1,26 +1,21 @@
 from supabase_py_async import AsyncClient
 
 from crewai_saas.crud.base import CRUDBase, ReadBase
-from crewai_saas.schema import EmployedCrew, EmployedCrewCreate, EmployedCrewUpdate
+from crewai_saas.schema import EmployedCrew, EmployedCrewCreate, EmployedCrewUpdate, Chat, ChatCreate, ChatUpdate
 from crewai_saas.schema.auth import UserIn
 
 class CRUDEmployedCrew(CRUDBase[EmployedCrew, EmployedCrewCreate, EmployedCrewUpdate]):
-    async def create(self, db: AsyncClient, *, obj_in: EmployedCrewCreate) -> EmployedCrew:
-        return await super().create(db, obj_in=obj_in)
+    pass
 
-    async def get(self, db: AsyncClient, *, id: int) -> EmployedCrew | None:
-        return await super().get(db, id=id)
+class CRUDChat(CRUDBase[Chat, ChatCreate, ChatUpdate]):
+    # 사이클이 생길 때마다 updated_at을 갱신한다.(최신순으로 정렬)
+    async def get_all_active_by_employed_crew_id(self, db: AsyncClient, *, employed_crew_id: int) -> list[Chat]:
+        data, count = await db.table(self.model.table_name).select("*").eq("employed_crew_id", employed_crew_id).eq("is_deleted", False).order("updated_at", desc=True).execute()
+        _, got = data
+        return [self.model(**item) for item in got]
 
-    async def get_all(self, db: AsyncClient) -> list[EmployedCrew]:
-        return await super().get_all(db)
 
-    async def get_multi_by_owner(self, db: AsyncClient, user_id: int) -> list[EmployedCrew]:
-        return await super().get_multi_by_owner(db, user_id=user_id)
 
-    async def update(self, db: AsyncClient, *, obj_in: EmployedCrewUpdate) -> EmployedCrew:
-        return await super().update(db, obj_in=obj_in)
-
-    async def delete(self, db: AsyncClient, *, id: int) -> EmployedCrew:
-        return await super().delete(db, id=id)
 
 empolyed_crew = CRUDEmployedCrew(EmployedCrew)
+chat = CRUDChat(Chat)
