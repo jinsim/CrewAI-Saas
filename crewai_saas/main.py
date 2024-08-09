546f73ad2f58532179f6b76b730aee19a1ee5d86
june0216 <ziyun1612@ewhain.net>
2024-08-09 20:55:14 +0900

    cors 에러 해결

--- crewai_saas/main.py
+++ crewai_saas/main.py
@@ -21,7 +21,7 @@ def create_app() -> FastAPI:
     if settings.BACKEND_CORS_ORIGINS:
         app.add_middleware(
             CORSMiddleware,
-            allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+            allow_origins=["*"],  # 모든 도메인 허용
             allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
