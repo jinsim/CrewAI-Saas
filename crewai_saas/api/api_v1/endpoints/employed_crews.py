90dab76d110cf3f9b0f7648a6bd16565a0d8ff57
june0216 <ziyun1612@ewhain.net>
2024-08-25 22:09:25 +0900

    employed crews get 수정

--- crewai_saas/api/api_v1/endpoints/employed_crews.py
+++ crewai_saas/api/api_v1/endpoints/employed_crews.py
@@ -59,9 +59,18 @@ async def read_employed_crews(user_id: Annotated[int, Path(title="The ID of the 
 
 @router.get("/by-user")
 async def read_employed_crews_by_user(session: SessionDep
-                              , user_email: str = Depends(GoogleAuthUtils.get_current_user_email)) -> list[EmployedCrew]:
-    get_user = await crud.user.get_active_by_email(session, email=user_email)
-    return await crud.employed_crew.get_all_active_employed_crews_by_owner(session, user_id=get_user.id)
+                              , user_email: str = Depends(GoogleAuthUtils.get_current_user_email)) -> list[EmployedCrewWithCrew]:
+    user = await crud.user.get_active_by_email(session, email=user_email)
+
+    employed_crews = await crud.employed_crew.get_all_active_employed_crews_by_owner(session, user_id=user.id)
+
+    results = []
+
+    for employed_crew in employed_crews:
+        crew = await crud.crew.get_active(session, id=employed_crew.crew_id)
+        results.append(EmployedCrewWithCrew(**employed_crew.dict(), crew=crew))
+
+    return results
 
 
 @router.get("/{employed_crew_id}")
