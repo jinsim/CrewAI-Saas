d143677bf4b3181de6b82b353b2a5879c287a343
june0216 <ziyun1612@ewhain.net>
2024-09-05 12:14:53 +0900

    test 용 employed crew 설정

--- crewai_saas/crud/crud_employed_crew.py
+++ crewai_saas/crud/crud_employed_crew.py
@@ -13,6 +13,15 @@ class CRUDEmployedCrew(CRUDBase[EmployedCrew, EmployedCrewCreate, EmployedCrewUp
     async def get_all_active_employed_crews_by_owner(self, db: AsyncClient, *, user_id: int) -> List[EmployedCrew]:
         query = db.table(self.model.table_name).select("*").eq("user_id", user_id).eq("is_deleted", False).order("created_at", desc=True)
         return await self._execute_multi_query(query)
+
+    async def get_all_active_by_crew_id_and_user_id(self, db: AsyncClient, *, crew_id: int, user_id: int) -> List[EmployedCrew]:
+        query = db.table(self.model.table_name).select("*").eq("crew_id", crew_id).eq("user_id", user_id).eq("is_deleted", False).order("created_at", desc=True)
+        return await self._execute_multi_query(query)
+
+    async def get_active_is_owned_by_crew_id_and_user_id(self, db: AsyncClient, *, crew_id: int, user_id: int) -> EmployedCrew:
+        query = db.table(self.model.table_name).select("*").eq("crew_id", crew_id).eq("user_id", user_id).eq("is_owner", True).eq("is_deleted", False)
+        return await self._execute_single_query(query)
+
     async def create_owned(self, db: AsyncClient, *, crew_id:int, user_id:int) -> EmployedCrew:
         data, count = await db.table(self.model.table_name).insert({"crew_id": crew_id, "user_id": user_id, "is_owner": True}).execute()
         _, got = data
@@ -56,8 +65,8 @@ class CRUDCycle(CRUDBase[Cycle, CycleCreate, CycleUpdate]):
         _, got = data
         return [self.model(**item) for item in got]
 
-    async def get_all_finished_by_chat_id(self, db: AsyncClient, *, chat_id: int) -> list[Cycle]:
-        data, count = await db.table(self.model.table_name).select("*").eq("chat_id", chat_id).eq("status", CycleStatus.FINISHED.value).order("id", desc=False).execute()
+    async def get_all_finished_and_started_by_chat_id(self, db: AsyncClient, *, chat_id: int) -> list[Cycle]:
+        data, count = await db.table(self.model.table_name).select("*").eq("chat_id", chat_id).in_("status", [CycleStatus.FINISHED.value, CycleStatus.STARTED.value]).order("id", desc=True).execute()
         _, got = data
         return [self.model(**item) for item in got]
 
